5636011adcc072c294a7869432eea1ab4a6e4c39
zangaru <154956071+zangaru@users.noreply.github.com>
2024-03-18 14:33:19 +0900

    [Bronze V] Title: 개, Time: 120 ms, Memory: 13884 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,10 @@
+/* " 출력 시, 이스케이프 시퀀스 \" 사용 */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
