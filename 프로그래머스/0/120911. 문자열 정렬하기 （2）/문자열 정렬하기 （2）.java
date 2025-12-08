aba2dcca7193c4e03bc8d8e339d59d2b8f338b57
zangaru <154956071+zangaru@users.noreply.github.com>
2025-12-08 14:09:37 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 2.34 ms, Memory: 71.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+    	
+    	my_string = my_string.toLowerCase();
+    	
+    	char[] arr = my_string.toCharArray();
+    	Arrays.sort(arr);
+    	
+    	return new String(arr);
+    }
+}
